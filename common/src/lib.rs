a6fcad9ad9ae51b0dd038b4b27072f6fd7b89ce2
Jonathan Fok kan <jonathan.fokkan@appdirect.com>
2018-12-07 16:59:16 -0500

    day 5 so far

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::fs;
 use std::io::Error;
 
-pub fn open_input() -> Result<String, Error> {
-    let input_file = env::args().nth(0).unwrap_or("input.txt".to_owned());
+pub fn open_input(default_file: &str) -> Result<String, Error> {
+    let input_file = env::args().nth(1).unwrap_or(default_file.to_owned());
     fs::read_to_string(input_file)
 }
--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,21 +1,101 @@
 extern crate common;
 
+use std::fmt;
+use std::fs;
+
 fn main() {
     println!("Hello, day 5!");
 
-    let input = common::open_input();
+
+    //let input = common::open_input("day5/input.txt").expect("failed to get input");
+
+    let input = fs::read_to_string("day5/input.txt").expect("failed to get input");
+
+    let mut particles = Particles::new(&input);
+    particles.react_completely();
+
+    println!("particles: {}", particles);
+    println!("len particles: {}", particles.particles.len());
 }
 
-fn run_reaction_cycle(particles: &str) -> String {
-    let mut remove_indexes = Vec::new();
-    particles.chars().peekable().enumerate().
+pub struct Particles {
+    pub particles: Vec<Particle>,
+}
+
+#[derive(Copy,Clone, Eq, PartialEq)]
+pub enum Particle {
+    Reacted,
+    UnReacted(char),
+}
+
+impl fmt::Display for Particle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Particle::Reacted => {
+                write!(f, "*")
+            }
+            Particle::UnReacted(ch) => {
+                write!(f, "{}", ch)
+            }
+        }
+    }
+}
+
+impl Particles {
+    pub fn new(input: &str) -> Particles {
+        Particles{
+            particles: input.chars().map(|c| Particle::UnReacted(c)).collect(),
+        }
+    }
+
+    pub fn react_once(&mut self) {
+        {
+        let mut iter = self.particles.iter_mut().peekable();
+        let mut remove_cur = false;
+        while let Some(current) = iter.next() {
+            if remove_cur {
+                *current = Particle::Reacted;
+                remove_cur = false;
+            } else {
+                if let Some(next) = iter.peek() {
+                    if let Particle::UnReacted(cur_char) = current.clone() {
+                        if let Particle::UnReacted(next_char) = next {
+                            if is_opposite(&cur_char, next_char) {
+                                *current = Particle::Reacted;
+                                remove_cur = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        }
+
+        self.particles = self.particles.clone().into_iter().filter(|x| *x != Particle::Reacted).collect();
+    }
+
+    pub fn react_completely(&mut self) {
+        let mut old = self.particles.clone();
+        self.react_once();
+        while old.len() != self.particles.len() {
+            old = self.particles.clone();
+            self.react_once();
+        }
+        self.react_once();
+    }
+}
+
+impl fmt::Display for Particles {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.particles.iter().map(|x| x.fmt(f)).collect()
+    }
 }
 
 // We can assume the inputs are ascii
-fn is_opposite(a: char, b: char) -> bool {
+fn is_opposite(a: &char, b: &char) -> bool {
     if a.is_lowercase() {
-        a.to_uppercase().nth(0).unwrap() == b
+        a.to_uppercase().nth(0).unwrap() == *b
     } else {
-        a.to_lowercase().nth(0).unwrap() == b
+        a.to_lowercase().nth(0).unwrap() == *b
     }
 }
